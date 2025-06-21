469f23e342075075bdaaefb63056696310db53b3
singhravi5590 <singhravi5590@gmail.com>
2025-06-21 18:59:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import Home from './pages/Home';
+import Movies from './pages/Movies';
+import MovieDetails from './pages/MovieDetails';
+import SeatLayout from './pages/SeatLayout';
+import MyBookings from './pages/MyBookings';
+import Favourite from './pages/Favourite';
+import Footer from './components/Footer'
+import {Toaster} from 'react-hot-toast';
+
+const App = () => {
+
+  const isAdminRoute = useLocation().pathname.startsWith('/admin');
+
+  return (
+    <>
+      <Toaster/>
+      { !isAdminRoute && <Navbar/>}
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/movies' element={<Movies/>}/>
+        <Route path='/movies/:id' element={<MovieDetails/>}/>
+        <Route path='/movies/:id/:date' element={<SeatLayout/>}/>
+        <Route path='/my-bookings' element={<MyBookings/>}/>
+        <Route path='/favourite' element={<Favourite/>}/>
+      </Routes>
+      {/* { !isAdminRoute && <Footer/>} */}
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HeroSection from '../components/HeroSection'
+import FeatureSection from '../components/FeatureSection'
+
+const Home = () => {
+  return (
+    <div>
+      {<HeroSection/>}
+      <FeatureSection/>
+    </div>
+  )
+}
+
+export default Home
