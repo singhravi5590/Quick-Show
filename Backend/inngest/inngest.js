0928fc00c4993f0a80abcdae5b944c78939b5a13
singhravi5590 <singhravi5590@gmail.com>
2025-06-28 11:29:39 +0530

    Deploying on vercel

--- Backend/inngest/inngest.js
+++ Backend/inngest/inngest.js
@@ -1,8 +1,10 @@
 import { Inngest } from "inngest";
+import User from "../model/user.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "movie-ticket-booking" });
 
+// Function to add user
 const syncUserCreation = inngest.createFunction(
     {id: 'sync-user-from-clerk'},
     {event: 'clerk/user.created'},
@@ -19,6 +21,7 @@ const syncUserCreation = inngest.createFunction(
     }
 )
 
+// function to delete user
 const syncUserDeletion = inngest.createFunction(
     {id : 'delete-user-with-clerk'},
     {event : 'clerk/user.deleted'},
@@ -27,11 +30,30 @@ const syncUserDeletion = inngest.createFunction(
         const {id} = event.data
         await User.findByIdAndDelete(id)
     } 
+)
 
+// function to update user
+
+const syncUserUpdation = inngest.createFunction(
+    {id : 'update-user-from-clerk'},
+    {event : 'clerk/user.updated'},
+
+    async({event}) => {
+        const {id, first_name, last_name, email_addresses, image_url} = event.data;
+
+        const userData = {
+            _id : id,
+            email : email_addresses[0].email_address,
+            name : first_name + " " + last_name,
+            image_url : image_url
+        }
+        await User.findByIdAndUpdate(id, userData);
+    }
 )
 
 // Create an empty array where we'll export future Inngest functions
 export const functions = [
     syncUserCreation, 
-    syncUserDeletion
+    syncUserDeletion,
+    syncUserUpdation
 ];
