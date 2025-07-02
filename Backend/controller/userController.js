6053e7c48d8d7fba6b20e5145cbadade986156d1
singhravi5590 <singhravi5590@gmail.com>
2025-07-02 16:49:14 +0530

    Connecting frontend and Backend

--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -1,4 +1,6 @@
-import Booking from "../model/Bookings";
+import Booking from "../model/Bookings.js";
+import { clerkClient } from "@clerk/express";
+import Movie from "../model/Movie.js";
 
 
 // API controller function to get user bookings
@@ -15,6 +17,62 @@ export async function getUserBookings(req, res){
         res.json({
             success : false,
             error : error.message
+        })
+    }
+}
+
+// API Controller function to update Favourite Movie in clerk User Metadata
+
+export async function updateFavourite(req, res){
+    try {
+        const {movieId} = req.body;
+        const {userId} = req.auth();
+
+        const user = await clerkClient.users.getUser(userId);
+
+        if(!user.privateMetadata.favourites){
+            user.privateMetadata.favourites = [];
+        }
+
+        if(!user.privateMetadata.favourites.includes(movieId)){
+            user.privateMetadata.favourites.push(movieId);
+        }
+        else{
+            user.privateMetadata.favourites = user.privateMetadata.favourites.filter((item) => item !== movieId)
+        }
+
+        await clerkClient.users.updateUserMetadata(userId, {privateMetadata : user.privateMetadata})
+        
+        res.json( {success : true, message : "Favourite Movie Updated Successfully"})
+    }
+     catch (error) {
+        res.json({
+            success : false,
+            error : error.message,
+        })
+    }
+}
+
+
+// API to get all the favourites movies
+
+export async function getFavourites(req, res){
+    try {
+        const user = await clerkClient.users.getUser(req.auth.userId);
+        const favourites = user.privateMetadata.favourites;
+
+        // Getting movie from database
+        const movies = await Movie.find({_id : {$in: favourites}});
+
+        res.json({
+            success : true,
+            movies
+        })
+    } 
+    catch (error) {
+        res.json({
+            success : false,
+            message : error.message
         })
     }
 }
---
+++ Frontend/src/context/AppContext.jsx
@@ -0,0 +1,96 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import axios from 'axios';
+import { useAuth, useUser } from "@clerk/clerk-react";
+import { useLocation, useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL
+
+
+export const AppContext = createContext();
+
+export const AppProvider = ({children}) => {
+
+    const [isAdmin, setIsAdmin] = useState(false);
+    const [shows, setShows] = useState([]);
+    const [favouriteMovies, setFavouriteMovies] = useState([]);
+
+    const {user} = useUser();
+    const {getToken} = useAuth();
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const fetchAdmin = async () => {
+        try {
+            const {data} = await axios.get('/api/admin/is-admin', {
+                headers : {
+                    Authorization : `Bearer ${await getToken()} `
+                }
+            })
+            setIsAdmin(data.isAdmin);
+            
+            if(!data.isAdmin && location.pathname.startsWith('/admin')){
+                navigate('/')
+                toast.error('You are not authorized to access admin dashboard')
+            }
+        } 
+        catch (error) {
+            console.log(error);
+        }
+    }
+
+    const fetchShows = async () => {
+        try {
+            const {data} = await axios.get('/api/show/all')
+            if(data.success){
+                setShows(data.shows);
+            }
+            else{
+                toast.error(data.message)
+            }
+        } 
+        catch (error) {
+            console.log(error)
+        }
+    }
+
+    const fetchFavouriteMovies = async () => {
+        try {
+            const {data} = await axios.get('/api/user/favourites', {headers : {
+                Authorization : `Bearer ${ await getToken()}`
+            }})
+            
+            if(data.success){
+                setFavouriteMovies(data.movies)
+            }
+            else{
+                toast.error(data.message)
+            }
+        } 
+        catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        fetchShows();
+    }, [])
+
+
+    useEffect(() => {
+        if(user){
+            fetchAdmin();
+            fetchFavouriteMovies();
+        }
+    }, [user])
+
+    value = {axios, fetchAdmin, user, getToken, navigate, isAdmin, shows, favouriteMovies, fetchFavouriteMovies}
+    return (
+        <AppContext.Provider value={value}>
+            {children}
+        </AppContext.Provider>
+    )
+}
+
+
+export const useAppContext =  () => useContext(AppContext);
